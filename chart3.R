76eb9ab455758614a7ad006616ed84a0f6cb45b3
Sam Wang <samw1@uw.edu>
2020-05-20 21:51:33 -0700

    fix lintr errors

--- chart3.R
+++ chart3.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(plotly)
 
-df <- read.csv("data/life-satisfaction-vs-life-expectancy.csv", stringsAsFactors = FALSE)
+df <- read.csv("data/life-satisfaction-vs-life-expectancy.csv",
+               stringsAsFactors = FALSE)
 
-df_filter <- df %>% 
+df_filter <- df %>%
   filter(!is.na(Life.expectancy..years.)) %>%
   select(-Total.population..Gapminder.)
 
@@ -14,8 +15,9 @@ avg_life_exp <- country_group %>%
   summarize(avg_life_exp = mean(Life.expectancy..years., na.rm = TRUE))
 
 avg_life_satisf <- country_group %>%
-  summarize(avg_life_satisf = mean(Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.,
-                                   na.rm = TRUE))
+  summarize(avg_life_satisf =
+mean(Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.,
+    na.rm = TRUE))
 
 avg <- merge(avg_life_exp, avg_life_satisf) %>%
   arrange(avg_life_exp)
@@ -28,8 +30,12 @@ life_exp_per_year <- year_group %>%
 
 y <- list(title = "Average Life Expetancy")
 
-plot_ly(life_exp_per_year, x=~Year, y=~avg_life_exp, mode="line",
-        text = ~paste("Year :", Year,
-                      "<br> Avg LE:", round(avg_life_exp, 2)),
-        hoverinfo = "text") %>%
+plot_ly(life_exp_per_year,
+  x = ~Year, y = ~avg_life_exp, mode = "line",
+  text = ~ paste(
+    "Year :", Year,
+    "<br> Avg LE:", round(avg_life_exp, 2)
+  ),
+  hoverinfo = "text"
+) %>%
   layout(yaxis = y)
