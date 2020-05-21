175e7a8f1b2cd453186027ad567fc2fdb4a7fcb4
Ryu Sharma <ryusei20@uw.edu>
2020-05-20 22:00:23 -0700

    Fix lintr

--- chart1.R
+++ chart1.R
@@ -1,23 +1,22 @@
-library(stringr)
 library(dplyr)
 library(ggplot2)
-library(plotly)
 
-df <- read.csv("data/life-satisfaction-vs-life-expectancy.csv", stringsAsFactors = FALSE)
 
-df_filter <- df %>% 
-  filter(!is.na(Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.)) %>% 
-  filter(!is.na(Life.expectancy..years.)) %>% 
-  select(-Total.population..Gapminder.) 
+df <- read.csv("data/life-satisfaction-vs-life-expectancy.csv",
+               stringsAsFactors = FALSE)
 
-country_group <- df_filter %>%
-  group_by(Entity)
+df <- rename(df, life_satis =
+   Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.)
 
-chart1 <- ggplot(data = df_filter, aes(group = Entity, 
-                 x = Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.,
-                 y = Life.expectancy..years.)) +
+df_filter <- df %>%
+  filter(!is.na(life_satis)) %>%
+  filter(!is.na(Life.expectancy..years.)) %>%
+  select(-Total.population..Gapminder.)
+
+chart1 <- ggplot(data = df_filter, aes(group = Entity,
+       x = life_satis,
+       y = Life.expectancy..years.)) +
        geom_boxplot() +
        labs(x = "Life Satisfaction",
-            y = "Life Expectancy") 
+            y = "Life Expectancy")
 chart1
-
