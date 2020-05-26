701284e07015b9241ed4066978c204eb8d84383a
Ryu Sharma <ryusei20@uw.edu>
2020-05-25 23:55:50 -0700

    Update chart to have comments

--- chart1.R
+++ chart1.R
@@ -1,12 +1,15 @@
+# Loads necessary libraries
 library(dplyr)
 library(ggplot2)
 
-
+# Reads the csv and turns it into a dataframe
 df <- read.csv("data/life-satisfaction-vs-life-expectancy.csv",
                stringsAsFactors = FALSE)
 
+# Creates a function used to generate a barplot chart 
 chart1 <- function(df) {
 
+  # Filters the initial dataframe to preserve only useful data
 df_filter <- df %>%
   filter(!is.na(
 Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.
@@ -14,6 +17,7 @@ Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.
   filter(!is.na(Life.expectancy..years.)) %>%
   select(-Total.population..Gapminder.)
 
+# Generates the boxplot based on data
 plot <- ggplot(data = df_filter, aes(group = Entity,
        x =
  Life.satisfaction..measured.from.lowest.0.to.highest.10.on.Cantril.Ladder.,
